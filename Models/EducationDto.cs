846b313a5101218582a6b3c7699ec3298cee70d3
namphu2911 <kuboy151@gmail.com>
2026-01-04 11:15:39 +0700

    update: homework lession 21 - layout porfolio

---
+++ Models/EducationDto.cs
@@ -0,0 +1,11 @@
+namespace web_blazor_server.Models
+{
+    public class EducationDto
+    {
+        public string SchoolName { get; set; } = "";
+        public string Degree { get; set; } = "";
+        public string Description { get; set; } = "";
+        public string TimeRange { get; set; } = "";
+        public string GPA { get; set; } = "";
+    }
+}
---
+++ Models/ExperienceDto.cs
@@ -0,0 +1,10 @@
+namespace web_blazor_server.Models
+{
+    public class ExperienceDto
+    {
+        public string JobTitle { get; set; } = "";
+        public string Company { get; set; } = "";
+        public string Description { get; set; } = "";
+        public string TimeRange { get; set; } = "";
+    }
+}
